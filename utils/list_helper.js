717b8a733f4b4521220377018c0da19db6057a6d
Jani Juvonen <jani.p.juvonen@gmail.com>
2025-05-19 13:50:41 +0300

    Exercise 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,15 @@
+
 const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const reducer = (acc, currentValue) => {
+    return acc + currentValue.likes
+  }
+  return blogs.reduce(reducer, 0)
+}
+
 module.exports = {
-    dummy
+    dummy, totalLikes
 }
